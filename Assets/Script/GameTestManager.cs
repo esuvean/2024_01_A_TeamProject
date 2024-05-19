723466812267387074123c4a3e61865885697a28
hehe1016 <msjhej486@gmail.com>
2024-05-20 01:59:17 +0900

    [TeamProject] 인터페이스 수정, 스크립트 생성

---
+++ Assets/Script/GameTestManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTestManager : MonoBehaviour
+{
+    public GameObject[] circleObject;                              //물체 프리팹을 가져온다.  (배열로 변경 =[])
+    public Transform genTransform;                                 //생성 위치 설정 
+    public float timeCheck;                                        //생성 시간 설정 변수 (flaot)
+    public bool isGen;                                             //생성 체크 (bbol)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
